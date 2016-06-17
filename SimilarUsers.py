6d2da740071087ab190968c950e859c828d2ea3d
Mohit <mohit@Mohits-MBP.fios-router.home>
2016-06-17 16:19:02 -0400

    Calculated String Similarity

--- SimilarUsers.py
+++ SimilarUsers.py
@@ -13,7 +13,7 @@ print(users_df.head())
           #print(users_df.head())
 users_df=users_df.iloc[0:len(users_df),1:len(users_df.columns)]  # Truncates the original dataframe to drop ID column
 
-olumns_to_drop=['object','name','password','image','token','bio','ts_deleted']
+columns_to_drop=['object','name','password','image','token','bio','ts_deleted']
 
 users_df=users_df.drop(columns_to_drop,axis=1)
 
@@ -23,15 +23,19 @@ newUser_df = pd.read_csv('new_user.csv')
 #List of numerical and categorical categories(columns)
 num_cat=['zip','last_login','last_active','user_type','created']
 str_cat=[]
+
+#Calculated similarity for each entry in users_df with newUser_df
+#and converted into a numeric dataframe to apply Machine Learning algorithms.
+#Used fuzzywuzzy which is based on calculating Levenshtein distance between Strings.
+#The similarity for already numeric categories (columns) were calculated based on Euclidean distance.
 for col in users_df.columns:
       ans=num_cat.index(col) if col in num_cat else None
       if ans is None:
            print "This is a string category "+col #Debug Note
-           print lv.distance(
-
+           users_df[col]=users_df.apply(lambda x:fuzz.ratio(str(x[col]),newUser_df[col].iat[0]),axis=1)
       else:
                print "This is a numerical category "+col #Debug Note
-
+     
 
 
 def toNumericalDataFrame(user_df)
